c515cd2708cbcd658795e9f6393dcd3f40fda481
gonzales <gonzalesitokawa@gmail.com>
2023-05-21 17:26:10 +0900

    start project

---
+++ src/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      's.gravatar.com'
+    ]
+  }
+}
+
+module.exports = nextConfig
---
+++ src/prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: true,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: "none",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: "always",
+  rangeStart: 0
+};
